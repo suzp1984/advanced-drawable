ab3fa2fc7c5d7764c66bde289c004b7d4bea9070
suzp1984 <suzp1984@gmail.com>
2017-05-07 23:00:42 +0800

    add CircleDrawable

--- app/src/main/java/io/github/suzp1984/advanceddrawables/MainActivity.kt
+++ app/src/main/java/io/github/suzp1984/advanceddrawables/MainActivity.kt
@@ -2,11 +2,19 @@ package io.github.suzp1984.advanceddrawables
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import io.github.suzp1984.advanceddrawables.drawable.CircleDrawable
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val image : ImageView = findViewById(R.id.image) as ImageView
+
+        val circle : CircleDrawable = CircleDrawable()
+        circle.setText("0")
+        image.setImageDrawable(circle)
     }
 }
---
+++ app/src/main/java/io/github/suzp1984/advanceddrawables/drawable/CircleDrawable.kt
@@ -0,0 +1,80 @@
+package io.github.suzp1984.advanceddrawables.drawable
+
+import android.graphics.*
+import android.graphics.drawable.Drawable
+import android.util.Log
+
+class CircleDrawable : Drawable() {
+
+    private var backgroundPaint: Paint = Paint()
+    private var txtPaint: Paint = Paint()
+    private var text: String = ""
+    private val TAG : String = "CircleDrawable"
+
+    init {
+        backgroundPaint.color = Color.rgb(255, 0, 0)
+
+        txtPaint.setTypeface(Typeface.DEFAULT)
+        txtPaint.color = Color.rgb(0, 0, 0)
+    }
+
+    override fun draw(canvas: Canvas?) {
+        if (canvas == null) {
+            return
+        }
+
+        val rect : Rect = bounds
+        canvas.drawCircle(rect.exactCenterX(), rect.exactCenterY(), rect.width() / 2.0f, backgroundPaint)
+
+        txtPaint.textSize = adjustTextSize()
+
+        val txtRect : Rect = Rect()
+        txtPaint.getTextBounds(text, 0, text.length, txtRect)
+        val txtWidth = txtPaint.measureText(text)
+        canvas.drawText(text, rect.exactCenterX() - txtWidth / 2, rect.exactCenterY() + txtRect.height() / 2, txtPaint)
+    }
+
+    override fun setAlpha(alpha: Int) {
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getOpacity(): Int {
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return PixelFormat.UNKNOWN
+    }
+
+    override fun setColorFilter(colorFilter: ColorFilter?) {
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    fun setText(t : String) {
+        text = t
+    }
+
+    private fun adjustTextSize() : Float {
+        val rect = bounds
+        val txtRect = Rect()
+
+        val min : Int = if (rect.width() < rect.height()) rect.width() else rect.height()
+
+        var txtSize = rect.width().toFloat()
+        Log.e(TAG, "rect width = " + rect.width())
+
+        while (true) {
+            txtPaint.setTextSize(txtSize)
+            txtPaint.getTextBounds(text, 0, text.length, txtRect)
+            val w = txtPaint.measureText(text)
+
+            val maxSize : Float = if (w > txtRect.height()) w else txtRect.height().toFloat()
+
+            Log.e(TAG, "text size = " + txtSize)
+            if (maxSize < (min * 8.0f / 10.0f)) {
+                break
+            }
+
+            txtSize -= 0.5f
+        }
+
+        return txtSize
+    }
+}
